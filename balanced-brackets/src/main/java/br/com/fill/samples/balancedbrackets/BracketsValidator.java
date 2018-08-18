f4b697efec739dcf803ad6918f894eccb62d0b67
Fillipe Soares <fillipeaguiar.soares@gmail.com>
2018-08-18 20:13:01 -0300

    Testes para validador. Implementação para passar os testes pendente.

---
+++ balanced-brackets/src/main/java/br/com/fill/samples/balancedbrackets/BracketsValidator.java
@@ -0,0 +1,12 @@
+package br.com.fill.samples.balancedbrackets;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BracketsValidator {
+
+	public boolean isValid(String cenario1) {
+		return false;
+	}
+
+}
--- balanced-brackets/src/test/java/br/com/fill/samples/balancedbrackets/BalancedBracketsApplicationTests.java
+++ balanced-brackets/src/test/java/br/com/fill/samples/balancedbrackets/BalancedBracketsApplicationTests.java
@@ -1,7 +1,11 @@
 package br.com.fill.samples.balancedbrackets;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +13,46 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class BalancedBracketsApplicationTests {
 
+	private static final String MSG_ERR_CENARIO_VALIDO = "Cenario %s não é válido";
+	private static final String MSG_ERR_CENARIO_INVALIDO = "Cenario %s  não é inválido";
+
+	private static final int TEMPO_LIMITE = 100;
+
+	@Autowired
+	private BracketsValidator validator;
+
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test(timeout=TEMPO_LIMITE)
+	public void testaCenariosBraviSucesso() {
+		String cenario1 = "(){}[]";
+		String cenario2 = "[{()}](){}";
+
+		assertTrue(String.format(MSG_ERR_CENARIO_VALIDO, cenario1), validator.isValid(cenario1));
+		assertTrue(String.format(MSG_ERR_CENARIO_VALIDO, cenario2), validator.isValid(cenario2));
+	}
+
+	@Test(timeout=TEMPO_LIMITE)
+	public void testaCenariosBraviFalha() {
+		String cenario1 = "[]{()";
+		String cenario2 = "[{)]";
+		
+		assertFalse(String.format(MSG_ERR_CENARIO_INVALIDO, cenario1), validator.isValid(cenario1));
+		assertFalse(String.format(MSG_ERR_CENARIO_INVALIDO, cenario2), validator.isValid(cenario2));
+	}
+
+	@Test(timeout=TEMPO_LIMITE)
+	public void testaCenarioVazio() {
+		assertFalse(String.format(MSG_ERR_CENARIO_INVALIDO, "STRING VAZIA"), validator.isValid(""));
+	}
+
+	@Test(timeout=TEMPO_LIMITE)
+	public void testaCaracteresInvalidos() {
+		String cenario1 = "a[{)]";
+		String cenario2 = "['{()}'](){}";
+		assertFalse(String.format(MSG_ERR_CENARIO_INVALIDO, cenario1), validator.isValid(cenario1));
+		assertFalse(String.format(MSG_ERR_CENARIO_INVALIDO, cenario2), validator.isValid(cenario2));
+	}
 }
