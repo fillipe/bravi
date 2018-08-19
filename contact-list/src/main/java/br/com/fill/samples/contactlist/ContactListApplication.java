c9865939354ed3e490a59af42190495c68a46b5b
Fillipe Soares <fillipeaguiar.soares@gmail.com>
2018-08-19 01:18:44 -0300

    Criação do projeto do segundo desafio

---
+++ contact-list/src/main/java/br/com/fill/samples/contactlist/ContactListApplication.java
@@ -0,0 +1,12 @@
+package br.com.fill.samples.contactlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactListApplication.class, args);
+	}
+}
